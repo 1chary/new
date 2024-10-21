34350e199cd75de544f957194e8641ef60f056c2
1chary <abhilashchary04@gmail.com>
2024-10-21 16:21:15 +0530

    object_instance

--- check.py
+++ check.py
@@ -1,9 +1,21 @@
-def to_check_weather_given_number_is_odd_or_even(n):
-    if n % 2 == 0:
+def get_the_fibonaci_series(n):
+    if (n == 0 or n == 1):
+        return n
+    else:
+        return get_the_fibonaci_series(n - 1) + get_the_fibonaci_series(n - 2)
+
+def to_check_the_number_is_prime_or_not(n):
+    count = 0
+    for i in range(2,n+1):
+        if n % i == 0:
+            count += 1
+    if count == 1:
         return True
     else:
         return False
 
-n = int(input())
-result = to_check_weather_given_number_is_odd_or_even(n)
-print(result)
+
+n = 9
+result = get_the_fibonaci_series(n)
+prime_number = to_check_the_number_is_prime_or_not(n)
+print(prime_number)
---
+++ first.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name,age) {
+        this.name = name,
+        this.age = age
+    }
+}
+
+
+let objectInstance = new Person("Abhilash",23)
+console.log(objectInstance)
